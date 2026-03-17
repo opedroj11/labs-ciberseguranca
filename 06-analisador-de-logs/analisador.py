ed9fa7087155a52f207f84e5460d570482f763b1
opedroj11 <pedro.galaxy7@gmail.com>
2026-03-17 19:25:05 -0300

    Update analisador.py

--- 06-analisador-de-logs/analisador.py
+++ 06-analisador-de-logs/analisador.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 def analisar_logs():
-    print("--- 🛡️ Analisador de Logs (SOC / Blue Team) ---\n")
+    print("---  Analisador de Logs (SOC / Blue Team) ---\n")
     
     arquivo_log = "server.log"
     limite_falhas = 5
@@ -21,22 +21,22 @@ def analisar_logs():
                         ip_suspeito = partes[2]
                         tentativas_falhas[ip_suspeito] += 1
 
-        print("📊 Relatório de Ameaças:")
+        print(" Relatório de Ameaças:")
         print("-" * 30)
         
         ataque_detectado = False
         
         for ip, quantidade_falhas in tentativas_falhas.items():
             if quantidade_falhas > limite_falhas:
-                print(f"🚨 ALERTA CRÍTICO: Ataque de Força Bruta detectado!")
+                print(f" ALERTA CRÍTICO: Ataque de Força Bruta detectado!")
                 print(f"   IP Invasor: {ip}")
                 print(f"   Tentativas bloqueadas: {quantidade_falhas} vezes")
                 ataque_detectado = True
             elif quantidade_falhas > 0:
-                print(f"⚠️ Aviso: O IP {ip} errou a senha {quantidade_falhas} vez(es). (Normal)")
+                print(f" Aviso: O IP {ip} errou a senha {quantidade_falhas} vez(es). (Normal)")
 
         if not ataque_detectado:
-            print("✅ Tudo limpo. Nenhum ataque massivo detectado.")
+            print(" Tudo limpo. Nenhum ataque massivo detectado.")
 
     except FileNotFoundError:
         print(f"[!] Erro: O arquivo {arquivo_log} não foi encontrado na pasta.")
