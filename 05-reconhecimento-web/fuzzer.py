f1f4e4b1fbe14892b61701d4de96b892c408a4fe
opedroj11 <pedro.galaxy7@gmail.com>
2026-03-17 18:51:05 -0300

    Update fuzzer.py

--- 05-reconhecimento-web/fuzzer.py
+++ 05-reconhecimento-web/fuzzer.py
@@ -8,20 +8,18 @@ def executar_laboratorio():
     print(f"🎯 Alvo: {alvo}")
     
     try:
-        # 1. CALIBRAGEM: Descobrindo o tamanho da página principal (Baseline)
         print("Calibrando o filtro de Falsos Positivos...")
         resposta_base = requests.get(alvo, timeout=5)
         tamanho_base = len(resposta_base.text)
         print(f"Tamanho padrão da página: {tamanho_base} bytes\n")
 
         with open("wordlist.txt", "r") as arquivo:
-            # Lendo as linhas e removendo duplicatas com 'set()' (Isso limpa aqueles 'admin' repetidos!)
             diretorios = set(arquivo.readlines())
             
             print("Iniciando o ataque...")
             for diretorio in diretorios:
                 diretorio_limpo = diretorio.strip()
-                if not diretorio_limpo: # Pula linhas em branco
+                if not diretorio_limpo:
                     continue
                     
                 url_teste = f"{alvo}{diretorio_limpo}"
@@ -30,14 +28,12 @@ def executar_laboratorio():
                     resposta = requests.get(url_teste, timeout=3)
                     tamanho_atual = len(resposta.text)
                     
-                    # 2. O FILTRO: Só mostra se for Status 200 E o tamanho for diferente do padrão
                     if resposta.status_code == 200:
                         if tamanho_atual != tamanho_base:
                             print(f"[+] ENCONTRADO (Tamanho: {tamanho_atual}b): {url_teste}")
-                        # Se for igual, ele silenciosamente ignora o falso positivo
                             
                 except requests.exceptions.Timeout:
-                    pass # Ignora erros de lentidão para não poluir a tela
+                    pass 
                 except requests.exceptions.RequestException:
                     pass
                     
